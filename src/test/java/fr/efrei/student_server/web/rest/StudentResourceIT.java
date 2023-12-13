2f3558e3847b7865c1f6f8ceeae1787470d6b37b
Léopold MAURICE <leopold.maurice@efrei.net>
2023-12-13 23:16:01 +0100

    implementation of integration tests

---
+++ src/test/java/fr/efrei/student_server/web/rest/StudentResourceIT.java
@@ -0,0 +1,82 @@
+package fr.efrei.student_server.web.rest;
+
+import fr.efrei.student_server.domain.Student;
+import fr.efrei.student_server.repository.StudentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class StudentResourceIT {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    private Student student;
+
+    @BeforeEach
+    void setUp() {
+        student = new Student();
+        student.setName("John Doe");
+        student.setAge(21);
+    }
+
+    @Test
+    @Transactional
+    void testGetStudent() {
+        studentRepository.save(student);
+        Student found = studentRepository.findById(student.getId()).orElse(null);
+
+        assertNotNull(found);
+        assertEquals("John Doe", found.getName());
+        assertEquals(21, found.getAge());
+    }
+
+    @Test
+    @Transactional
+    void testCreateStudent() {
+        long initialCount = studentRepository.count();
+
+        studentRepository.save(new Student());
+        long newCount = studentRepository.count();
+
+        assertEquals(initialCount + 1, newCount);
+    }
+
+    @Test
+    @Transactional
+    void testUpdateStudent() {
+        studentRepository.save(student);
+
+        student.setName("Jane Doe");
+        studentRepository.save(student);
+
+        Student updated = studentRepository.findById(student.getId()).orElse(null);
+        assertNotNull(updated);
+        assertEquals("Jane Doe", updated.getName());
+    }
+
+    @Test
+    @Transactional
+    void testDeleteStudent() {
+        studentRepository.save(student);
+        assertNotNull(studentRepository.findById(student.getId()));
+
+        studentRepository.deleteById(student.getId());
+        assertFalse(studentRepository.findById(student.getId()).isPresent());
+    }
+
+    @Test
+    @Transactional
+    void testNonExistingStudent() {
+        assertFalse(studentRepository.findById(-1).isPresent());
+    }
+}
